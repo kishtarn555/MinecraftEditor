e8db371b32c3b8d610f1535dcdb0324cd5688817
kishtarn555 <hec_montemoon@hotmail.com>
2022-10-16 12:19:48 -0500

    sub

---
+++ Submodules/BedrocKTools/src/Nbt/Elements/NbtElement.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using BedrockTools.Nbt.Util;
+namespace BedrockTools.Nbt.Elements {
+    public abstract class NbtElement {
+        public abstract NbtTag Tag { get; }
+        public void WriteTag(BinaryWriter writer) {
+            writer.Write((byte)Tag);
+        }
+        public abstract void WriteValue(BinaryWriter writer);
+
+
+    }
+}
---
+++ Submodules/BedrocKTools/src/Structure/Template/TemplateBlock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BedrockTools.Nbt.Elements;
+using BedrockTools.Objects.Blocks;
+
+namespace BedrockTools.Structure.Template {
+    public class TemplateBlock : Block {
+        public readonly int TemplateIndex;
+
+        public static string StringIdentifier="bedrocktools:template";
+        public TemplateBlock(int index) : base(StringIdentifier) {
+            TemplateIndex = index;
+        }
+
+        public override NbtCompoundSorted GetBlockState() {
+            return new NbtCompoundSorted() { { "template_index", (NbtInt)TemplateIndex } };
+        }
+
+        public override NbtCompoundOrdered GetStructureBlock() {
+            throw new NotImplementedException("Template block is a aux block, it is not meant to be used as an actual block");
+        }
+        public override bool Equals(object obj) {
+            if (obj is TemplateBlock template) {
+                return TemplateIndex == template.TemplateIndex;
+            }
+            return false;
+        }
+
+        public override int GetHashCode() => HashCode.Combine(Identifier, TemplateIndex);
+    }
+}
